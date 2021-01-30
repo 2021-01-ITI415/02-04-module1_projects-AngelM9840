593abc3ad741b066475e535d36aada8f97ac82ea
AngelM9840 <amsantiago40@gmail.com>
2021-01-30 16:52:09 -0500

    Apple tree script change
    Apple tree script given movement and ability to randomly change direction

--- Assets/01-Apple Picker/Scripts/AppleTree.cs
+++ Assets/01-Apple Picker/Scripts/AppleTree.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class AppleTree : MonoBehaviour{
     [Header("Set in Inspector")]
-    
+
     public GameObject applePrefab;
 
     public float speed = 1f;
@@ -22,8 +22,23 @@ public class AppleTree : MonoBehaviour{
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        if (pos.x < -leftandRightEdge ) {
+            speed = Mathf.Abs(speed);
+        } else if  (pos.x > leftandRightEdge ) {
+            speed = -Mathf.Abs(speed);
+        } 
+    }
+
+    void FixedUpdate() {
+
+        if ( Random.value < chancetoChangeDirections ) {
+            speed *= -1;
+        }
     }
 }
