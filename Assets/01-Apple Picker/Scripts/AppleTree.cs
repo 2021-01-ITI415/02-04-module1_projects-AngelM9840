1fd436bb6a2af33afb0a5ca0534cd670af01a2c0
AngelM9840 <amsantiago40@gmail.com>
2021-01-30 19:09:28 -0500

    Basket Destruction and High Score
    Basket is destroyed when apple is not collect and high score script has been created

--- Assets/01-Apple Picker/Scripts/AppleTree.cs
+++ Assets/01-Apple Picker/Scripts/AppleTree.cs
@@ -6,13 +6,9 @@ public class AppleTree : MonoBehaviour{
     [Header("Set in Inspector")]
 
     public GameObject applePrefab;
-
     public float speed = 1f;
-
     public float leftandRightEdge = 10f;
-
     public float chancetoChangeDirections = 0.1f;
-
     public float secondsBetweenAppleDrops = 1f;
 
     // Start is called before the first frame update
